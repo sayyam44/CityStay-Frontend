7a6d406bae0afb7723d4bc3ca6d89de7fffad8ad
TicTacToe <sayyamkundra52@gmail.com>
2025-03-19 00:06:35 -0230

    frontend_updates

--- src/components/Register.js
+++ src/components/Register.js
@@ -252,7 +252,7 @@ function Register() {
     useEffect(()=>{
         if (state.openSnack){
             setTimeout(()=>{
-                navigate("/");
+                navigate("/login");
             },1500);
         }
     },[state.openSnack])
